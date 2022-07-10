3424a2bbb61ce6027b9d3550e43b233e02b7f26d
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-07-10 13:47:56 -0700

    Conectar XML con logica gracias al metodo findViewBYId

--- people_counter/src/main/java/noe/basic/people_counter/MainActivity.kt
+++ people_counter/src/main/java/noe/basic/people_counter/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+var timesClicked =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val btnCounterK = findViewById<Button>(R.id.btn_counter)
+        val txtCounterK = findViewById<TextView>(R.id.txt_counter)
+
         //Si deseas llamar algun elemento estatico de la clase View usa esta forma
         btnCounterK.setOnClickListener(
             View.OnClickListener {
@@ -20,6 +25,10 @@ class MainActivity : AppCompatActivity() {
         //si no
         btnCounterK.setOnClickListener {
             Log.w(this.localClassName,"sin recurrir a metodo estatico de View")
+            this.timesClicked +=1;
+            txtCounterK.setText("He sido presionado: "+this.timesClicked+" veces")
+            Toast.makeText(this,"presionaste el boton",Toast.LENGTH_SHORT).show()
+
         }
 
 
